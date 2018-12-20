133fbbd166de0d903242132728f5408807493ba2
andrej.ljashok <andrej.ljashok@stfalcon.com>
2018-12-20 17:43:38 +0200

    simple form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,128 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="progress">
+      <div class="determinate" :style="progressWidth"></div>
+    </div>
+    <div class="hello">
+      <div class="row">
+        <form   class="col s12"
+                v-if="!showForm"
+                @submit.prevent="showForm = true">
+          <div class="row">
+            <div class="input-field col s12"
+                 v-for="(item, index) in info"
+                 :key="index">
+              <input
+                      class="validate"
+                      :id=item.name
+                      type="text"
+                      :value="item.value"
+                      @input="onInput(index ,$event.target.value)">
+              <label
+                      :for=item.name >{{item.name}}
+                <i v-if="controls[index].activated"
+                   :class="[`fa-${controls[index].error ? 'exclamation' : 'check'}-circle`, controls[index].error ? 'red-text' : 'green-text']"
+                   class="fas">
+                </i>
+              </label>
+            </div>
+          </div>
+          <div class="row">
+            <button class="waves-effect waves-light btn" :disabled="done < info.length">Send data</button>
+          </div>
+        </form>
+        <table v-else>
+          <tbody>
+          <tr
+                v-for="(item, index) in info"
+                :key="index">
+            <td><b>{{item.name}}</b></td>
+            <td>{{item.value}}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  index: 'TestVue',
   props: {
     msg: String
+  },
+  data: function () {
+      return {
+          info: [
+              {
+                  name: 'Name',
+                  value: '',
+                  pattern: /^[a-zA-Z]{2,30}$/,
+              },
+              {
+                  name: 'Phone',
+                  value: '',
+                  pattern: /^[0-9]{7,14}$/,
+              },
+              {
+                  name: 'Email',
+                  value: '',
+                  pattern: /.+/,
+              },
+              {
+                  name: 'Some Field',
+                  value: '',
+                  pattern: /.+/,
+              },
+          ],
+          controls: [],
+          showForm: false,
+      }
+  },
+  beforeMount() {
+    for(let i = 0; i < this.info.length; i++) {
+        this.controls.push({
+            error: true,
+            activated: false
+        })
+    }
+  },
+    computed: {
+        done() {
+            let done = 0;
+            for (let i = 0; i < this.controls.length; i++) {
+                (!this.controls[i].error) ? done++ : '';
+            }
+            return done
+        },
+        progressWidth() {
+            return {
+                width: (this.done / this.info.length * 100 ) + '%'
+            }
+        }
+  },
+  methods: {
+     onInput (index, value) {
+         let data = this.info[index];
+         let control = this.controls[index];
+
+         data.value = value;
+         control.error = !data.pattern.test(value);
+         this.controls[index].activated = true;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  .container {
+    max-width: 1000px;
+    margin: auto;
+  }
+
 h3 {
   margin: 40px 0 0;
 }
@@ -49,7 +131,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
